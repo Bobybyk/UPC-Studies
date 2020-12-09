b968e8f44159c8e5a7b6c5cb81e71608198b7d4e
Matthieu Le Franc <matt.lefranc@gmail.com>
2020-12-09 12:35:37 +0100

    UE modif + TP AAL add + POO TP11 almost done

--- Licence_Informatique/L2/S3/POO/TP11/TabSet.java
+++ Licence_Informatique/L2/S3/POO/TP11/TabSet.java
@@ -1,18 +1,18 @@
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Set;
 
-public class TabSet<E> implements Iterable<E>{
+public class TabSet<E> implements Iterable<E>, Set<E>{
     private E[] tableau;
 
     @SuppressWarnings("unchecked")
     public TabSet(int n) {
-        for (int i = 0 ; i <= n ; i++) {
-            this.tableau = (E[]) new Object[n];
-        }
+        this.tableau = (E[]) new Object[n];
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new TabIter(tableau);
     }
 
     public boolean contains (Object o) {
@@ -20,12 +20,19 @@ public class TabSet<E> implements Iterable<E>{
         if (tableau[0] != null && tableau[0].equals(o)) {
             return true;
         }
+
         while (ite.hasNext()) {
-            if (ite.next().equals(o)) {
+            if (ite.next() == o) {
                 return true;
             }
         }
         return false;
+    }
+
+    public void afficher() {
+        for (int i = 0 ; i<tableau.length ; i++) {
+            System.out.println(tableau[i]);
+        }
     }
 
     public int size() {
@@ -35,7 +42,7 @@ public class TabSet<E> implements Iterable<E>{
             tmp++;
         }
         while(ite.hasNext()) {
-            if (ite.next().equals(null)) {
+            if (ite.next() != null) {
                 tmp++;
             } 
         }
@@ -48,7 +55,7 @@ public class TabSet<E> implements Iterable<E>{
         } return false;
     }
 
-    public boolean add(E e) throws IllegalStateException {
+    public boolean add(E e) {
         if (!contains(e)) {
             if (e != null) {
                 Iterator<E> ite = iterator();
@@ -56,6 +63,90 @@ public class TabSet<E> implements Iterable<E>{
                 return true;
             }
         } return false;
+    }
+
+    public boolean remove(Object o) {
+        Iterator<E> ite = iterator();
+        if (tableau[0] == o) {
+            ite.remove();
+        }
+        
+        if (contains(o)) {
+            while(ite.hasNext()) {
+                if (ite.next() == o) {
+                    ((TabIter) ite).remove();
+                    return true;
+                }
+            }
+        } return false;
+    }
+
+    public void clear() {
+        Iterator<E> ite = iterator();
+        ite.remove();
+        while(ite.hasNext()) {
+            ite.next();
+            ite.remove();
+        }
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] t = new Object[size()];
+        int tmp = 0;
+        for (Object o : t) {
+            if (o != null) {
+                t[tmp] = o;
+                tmp++;
+            }
+        } 
+        return t;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        for (Object o : a) {
+            if (o != null) {
+                //this.tableau[index] = a[o];
+            }
+            
+        }
+        return null;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        for (E element: c) {
+            add(element);
+        } return containsAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        for (Object element: c) {
+            if (!contains(element)) {
+                remove(element);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        for (Object element: c) {
+            remove(element);
+        }
+        return !containsAll(c);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object element: c) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public class TabIter implements Iterator<E> {
@@ -67,21 +158,22 @@ public class TabSet<E> implements Iterable<E>{
         }
 
         public void add(E e) throws IllegalStateException{
-            while (hasNext()) {
-                if (next() == null) {
-                    tableau[index] = e;
-
-                }
+            if (tableau[0] == null) {
+                tableau[0] = e;
             }
-            throw new IllegalStateException();
+            else {    
+                while (hasNext()) {
+                    if (next() == null) {
+                        tableau[index] = e;
+                        return;
+                    }
+                }
+                throw new IllegalStateException();
+            }
         }
 
-        public boolean remove(Object o) {
-            if (contains(o)) {
-                Iterator<E> ite = iterator();
-                ((TabIter) ite).remove();
-                return true;
-            } return false;
+        public void remove() {
+            tableau[index] = null;
         }
 
         @Override
