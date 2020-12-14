fe579fecdef823a145ff7970c23b8cd4c47facc7
Matthieu Le Franc <matt.lefranc@gmail.com>
2020-12-14 20:09:45 +0100

    révisions

---
+++ Licence_Informatique/L2/S3/POO/Révisions/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        Voiture v = new Ferrari();
+        v.affiche();    // 
+        Voiture.sAffiche();
+        
+    }
+}
---
+++ Licence_Informatique/L2/S3/POO/Révisions/Voiture.java
@@ -0,0 +1,10 @@
+public class Voiture {
+
+    public void affiche() {
+        System.out.println("Je suis une voiture");
+    }
+
+    public static void sAffiche() {
+        System.out.println("Je suis une Voiture");
+    }
+}
