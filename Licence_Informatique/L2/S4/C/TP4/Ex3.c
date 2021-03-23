e51fbc64e70237b2c300b36d5fa10d0aec2454bc
Matthieu Le Franc <matt.lefranc@gmail.com>
2021-03-23 17:32:21 +0100

    C TP4 Ex3

--- Licence_Informatique/L2/S4/C/TP4/Ex3.c
+++ Licence_Informatique/L2/S4/C/TP4/Ex3.c
@@ -15,8 +15,8 @@ void array_print(array *t) {
 }
 
 array *array_init(int n) {
-	array *t = malloc(sizeof(int) * (n+1));
-
+	array *t = malloc(sizeof(array));
+	t->ptr = malloc(sizeof(int) * n);
 	return t;
 }
 
@@ -34,14 +34,39 @@ int array_set(array *t, int index, int valeur) {
 	t->ptr[index] = valeur;
 }
 
+int array_insert(array *t, int index, int valeur) {
+	array *tmp = malloc(sizeof(array));
+	assert(tmp != NULL);
+	tmp->ptr = malloc(sizeof(int) * (t->size + 1));
+	assert(tmp->ptr != NULL);
+	for (int i = 0 ; i < tmp->size ; i++) {
+		if (i == index) {
+			tmp->ptr[i] = valeur;
+			printf("Hello\n");
+		}
+		else {
+			tmp->ptr[i] = t->ptr[i];
+		}	
+		printf("OK\n");
+	}
+	t = realloc(tmp->ptr, sizeof(int) * (t->size+1));
+	t->ptr = tmp->ptr;
+	t->size = tmp->size;
+	free(tmp);
+	return 1;
+}
+
 int main() {
 	array *t = malloc(sizeof(array));
 	int tab[10] = {0,1,2,3,4,5,6,7,8,9};
 	t->ptr = tab;
 	t->size = 10;
 
-	array_print(t); 
-	printf("------");
-	array_print(array_init(5));
+	//array_print(t); 
+	printf("------\n");
+	//array_print(array_init(5));
+	array_print(t);
+	array_insert(t, 2, 18);
+	array_print(t);
 	return 0;
 }
