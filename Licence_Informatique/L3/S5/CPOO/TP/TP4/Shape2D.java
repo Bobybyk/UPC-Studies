009535f050d7f7cb3e2685d24a3e9c62af224085
Matthieu Le Franc <matt.lefranc@gmail.com>
2021-11-15 09:28:31 +0100

    Djikstra

---
+++ Licence_Informatique/L3/S5/CPOO/TP/TP4/Shape2D.java
@@ -0,0 +1,10 @@
+public interface Shape2D {
+	double perimeter();
+	double surface();
+
+	/**
+	* Méthode servant juste au test. Ne doit pas servir dans le programme final.
+	* @return true si la figure a bien les propriétés qu'elle prétend avoir
+	*/
+	default boolean checkInvariants() { return true; }
+}
---
+++ Licence_Informatique/L3/S5/CPOO/TP/TP4/Shape2DDecoree.java
@@ -0,0 +1,15 @@
+public class Shape2DDecoree implements Shape2D {
+
+    @Override
+    public double perimeter() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double surface() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
