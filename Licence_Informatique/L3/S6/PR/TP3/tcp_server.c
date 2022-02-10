257da69e48b3455d353d3942aa5781fd14cdda48
LE FRANC matthieu <lefrancm@informatique.univ-paris-diderot.fr>
2022-02-10 16:22:29 +0100

    PR TP3 ex1

--- Licence_Informatique/L3/S6/PR/TP3/tcp_server.c
+++ Licence_Informatique/L3/S6/PR/TP3/tcp_server.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define PORT 6666
 #define MAX 80
@@ -13,10 +14,14 @@
 
 void listener(int conn_fd) {
 	char buff[MAX];
+	int n;
 
 	while(1) {
 		bzero(buff, MAX);
 		read(conn_fd, buff, sizeof(buff));
+		n = 0;
+        // copy server message in the buffer
+        while ((buff[n++] = getchar()) != '\n');
 		write(conn_fd, buff, sizeof(buff));
 	}
 	close(conn_fd);
@@ -29,7 +34,12 @@ int main(int argc, char **argv) {
 
 	//socket creation/verification
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	assert(socket_fd >= 0);
+    if (socket_fd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else {
+		printf("Socket successfully created..\n");
+	}
 	bzero(&servaddr, sizeof(servaddr));
 
 	// assing IP, PORT
@@ -40,16 +50,28 @@ int main(int argc, char **argv) {
 	// bindling newly created socket to given IP and verification
     ret = bind(socket_fd, (SA*)&servaddr, sizeof(servaddr));
 
-	if (ret == 0) {
-		ret = listen(socket_fd, 0);
-		if (ret != 0) {
-			len = sizeof(cli);
-			conn_fd = accept(socket_fd, (SA*)&cli, &len);
-			if (conn_fd > -1) {
-				listener(conn_fd);
-			}
-		}
+	if (ret != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
+	} else {
+		printf("Socket successfully binded..\n");
 	}
+	ret = listen(socket_fd, 5);
+	if (ret != 0) {
+	    printf("Listen failed...\n");
+        exit(0);
+	} else {
+		printf("Server listening..\n");
+	}
+	len = sizeof(cli);
+	conn_fd = accept(socket_fd, (SA*)&cli, &len);
+	if (conn_fd < 0) {
+        printf("server accept failed...\n");
+        exit(0);
+	} else {
+		printf("server accept the client...\n");
+	}
+	listener(conn_fd);
 	close(socket_fd);
 	return 0;
 }
