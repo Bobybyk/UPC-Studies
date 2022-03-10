c4cfc5d50de831cfa20c4fa4521b8bef2715cb31
Sir Henry Allen <matt.lefranc@gmail.com>
2022-03-10 14:29:05 +0100

    edit client

--- Licence_Informatique/L3/S6/PR/TPs/TP6/ClientDaytime.java
+++ Licence_Informatique/L3/S6/PR/TPs/TP6/ClientDaytime.java
@@ -1,3 +1,12 @@
 public class ClientDaytime {
-    
+    ServerSocket socketServer;
+    Socket socketduserveur;
+    final BufferedReader in;
+    final PrintWriter out;
+    final Scanner sc;
+
+    public ClientDaytime() {
+        this.socketServer = 
+        sc=new Scanner(System.in);
+    }
 }
