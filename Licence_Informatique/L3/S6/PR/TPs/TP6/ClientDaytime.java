ca240b6eb31f979fda976a6c46de12b512bf337d
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-03-10 15:25:49 +0100

    PR TP6 ex1

--- Licence_Informatique/L3/S6/PR/TPs/TP6/ClientDaytime.java
+++ Licence_Informatique/L3/S6/PR/TPs/TP6/ClientDaytime.java
@@ -1,12 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientDaytime {
-    ServerSocket socketServer;
-    Socket socketduserveur;
-    final BufferedReader in;
-    final PrintWriter out;
-    final Scanner sc;
+        private Socket clientSocket;
+        private BufferedReader in;
+        private PrintWriter out;
+        private Scanner sc = new Scanner(System.in);//pour lire à partir du clavier
 
     public ClientDaytime() {
-        this.socketServer = 
-        sc=new Scanner(System.in);
+        
+        try {
+            this.clientSocket = new Socket("192.168.70.237",13);
+            //flux pour envoyer
+            this.out = new PrintWriter(clientSocket.getOutputStream());
+            //flux pour recevoir
+            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String daytimeMessage = in.readLine();
+
+            System.out.println(daytimeMessage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ClientDaytime();
     }
 }
