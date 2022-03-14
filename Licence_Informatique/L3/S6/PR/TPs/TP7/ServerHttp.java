834ec11732bae79e7b3c305b8da46e88aab058c5
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-03-14 15:05:54 +0100

    ajout de cours pdf info (unity, rpi, machine learning...)

---
+++ Licence_Informatique/L3/S6/PR/TPs/TP7/ServerHttp.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerHttp implements Runnable {
+    private ServerSocket socketServer;
+    private Socket socketduserveur;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public ServerHttp() {
+        try {
+            this.socketServer = new ServerSocket(8080);
+            this.socketduserveur = socketServer.accept();
+            this.in = new BufferedReader(new InputStreamReader(socketduserveur.getInputStream()));
+            this.out = new PrintWriter(socketduserveur.getOutputStream());
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public String formatageFichierHtml() {
+
+        String strToReturn = "";
+        
+        // ouverture du fichier
+		File f = new File("ex2.html");
+
+		/*
+         * lecture du fichier et ajout de chaque ligne et 
+         * de sa séquence de fin dans la chaine à retourner
+         */
+		try {
+			try (Scanner sc = new Scanner(f)) {
+				while (sc.hasNextLine()) {
+					strToReturn += (sc.nextLine());
+                    strToReturn += "\r\n";
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+        return strToReturn;
+    }
+
+    public String listen() {
+        
+        boolean endSequenceRead = false;
+        int line = 0;
+        String messageClient;
+
+        while(!endSequenceRead) {
+
+            try {
+                System.out.print(line + " : ");
+                messageClient = in.readLine();
+                if (messageClient.isEmpty()) {
+                    endSequenceRead = true;
+                }
+                System.out.println(messageClient);
+                line++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return formatageFichierHtml();
+    }
+
+    @Override
+    public void run() {
+        String messageToSend = this.listen();
+        System.out.println(messageToSend);
+        this.out.write(messageToSend);
+        System.out.println("OK");
+    }
+
+    public static void main(String[] args) {
+        
+        ServerHttp server = new ServerHttp();
+        server.run();
+        
+        try {
+            server.socketServer.close();
+            server.socketduserveur.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
