ad89f138a32c460066f1de582ab3bf97f30427f8
LE FRANC matthieu <matt.lefranc@gmail.com>
2022-09-22 17:46:06 +0200

    PM TP2

---
+++ Master1_IMPAIRS/S1/PM/TPs/TP2/app/src/main/java/com/pm/tp2/MultiplyActivity.kt
@@ -0,0 +1,12 @@
+package com.pm.tp2
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MultiplyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiplu)
+    }
+}
