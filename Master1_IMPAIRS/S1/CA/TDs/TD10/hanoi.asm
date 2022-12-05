851f0e6ab8f7fe6d3371dd9f896d9a57d013aa21
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-12-05 17:14:10 +0100

    hanoi asm eg + pr cours11

--- Master1_IMPAIRS/S1/CA/TDs/TD10/hanoi.asm
+++ Master1_IMPAIRS/S1/CA/TDs/TD10/hanoi.asm
@@ -1,91 +1,98 @@
-;; Convention d'appel:
-;;	- arguments passés sur la pile, avec le premier au sommet, 
-;;	- le suivant et ainsi de suite
-;; 	- R7 est sauvegardé par l'appelant
-;;	- R0-R5 sont sauvegardés par l'appelé
+        ;; On suit la convention d'appel suivante :
+        ;;   - les arguments sont passés sur la pile, empilés de droite à gauche
+        ;;   - R7 est caller-save, R1-R5 sont callee-save
 
-.ORIG x3000
-LEA R6, stbot
+        .ORIG x3000
 
-;; mvt(src, tgt)
-mvt:	
-	;; prologue
-	ADD R6, R6, -3
-	STR R0, R6, 0
-	STR R1, R6, 1
-	STR R7, R6, 2
+        LEA R6, stbot
 
-	;; code de mouvement
-	LD R1, char0
-	LDR R0, R6, 3
-	ADD R0, R0, R1		; R0 <- src + '0'
-	TRAP x21		; putc(src + '0')
-	LEA R0, arrow
-	TRAP x22		; printf(" --> ");
-	LDR R0, R6, 4
-	ADD R0, R0, R1		; R0 <- tgt + '0'
-	TRAP x21		; putc(tgt + '0')
-	LD R0, charNL
-	TRAP x21		; putc('\n')
+        ;; Note : on considère un peu arbitrairement que la fonction principale
+        ;; n'a pas à sauvegarder les registres.
 
-	;; épilogue
-	LDR R0, R6, 0
-	LDR R1, R6, 1
-	LDR R7, R6, 2
-	RET
-char0: .FILL 48		; 48 = '0'
-charNL: .FILL 10
-arrow: .STRINGZ " ---> "
+main:   AND R0, R0, 0
+        ADD R0, R0, 3
+        ADD R6, R6, -3
+        STR R0, R6, 0           ; arg0 <- 3
+        STR R0, R6, 2           ; arg2 <- 3
+        ADD R0, R0, -2
+        STR R0, R6, 1           ; arg1 <- 1
+        JSR hanoi              ; hanoi(3, 1, 3)
+        ADD R6, R6, 3           ; pop arg1, arg2, arg3
 
-;; hanoi(nb_disques, src, tqt)
-hanoi:
-	;; prologue
-	ADD R6, R6, -5
-	STR R0, R6, 0
-	STR R1, R6, 1
-	STR R2, R6, 2
-	STR R3, R6, 3
-	STR R7, R6, 4
-	
-	LDR R0, R6, 5		; R0 <- nb_disques
-	BRnz hanoi0		; if (nb_disques <= 0) goto hanoi0
+        AND R0, R0, 0
+        TRAP x25                ; exit(0)
 
-	ADD R0, R0, -1		; nb_disques <- nb_disques - 1
-	LDR R1, R6, 6		; R1 <- src
-	LDR R2, R6, 7		; R2 <- tgt
-	ADD R3, R1, R2
-	NOT R3, R3
-	ADD R3, R3, 7		; itm = R3 <- 6 - src - tgt
+        ;; Fonction mvt(src, dst)
+mvt:    ADD R6, R6, -3
+        STR R0, R6, 0
+        STR R1, R6, 1
+        STR R7, R6, 2           ; prologue
 
-	ADD R6, R6, -3
-	STR R3, R6, 2
-	STR R1, R6, 1
-	STR R0, R6, 0
-	JSR hanoi 		; hanoi(nb_disques - 1, src, itm)
+        LD R1, char0
+        LDR R0, R6, 3
+        ADD R0, R0, R1          ; R0 <- arg0 + '0'
+        TRAP x21
+        LEA R0, arrow
+        TRAP x22
+        LDR R0, R6, 4           ; R0 <- arg1 + '0'
+        ADD R0, R0, R1
+        TRAP x21
+        LD R0, charNL           ; R0 <- '\n'
+        TRAP x21
 
-	ADD R6, R6, -2
-	STR R2, R6, 1
-	STR R1, R6, 0
-	JSR mvt			; mvt(src, tgt)
+mvt0:   LDR R7, R6, 2           ; épilogue
+        LDR R1, R6, 1
+        LDR R0, R6, 0
+        ADD R6, R6, 3
+        RET
 
-	STR R2, R6, 2
-	STR R3, R6, 1
-	STR R0, R6, 0
-	JSR hanoi		; hanoi(nb_disques -1, itm, tgt)
-	ADD R6, R6, 3 
-	 
-hanoi0:
-	
-	ADD R6, R6, 5
-	RET
-;; On suppose que main n'a pas besoin de préserver les registres
-main:
-	AND R0, R0, 0
-	ADD R0, R0, 3
-	STR R0, R6, 2		; R[2] <- 1
-	ADD R0, R6, 0		; R[0] <- 		
-	STR R0, R6, 		; R6[1] <- 3
-	JSR hanoi		; hanoi(3, 1, 3)
-	ADD R6, R6, 2
-sttop:	.BLKW 100
-stbot:	.END	
+        ;; Fonction hanoi(nb_disques, src, dst)
+hanoi:  ADD R6, R6, -4
+        STR R7, R6, 0
+        STR R0, R6, 1
+        STR R1, R6, 2
+        STR R2, R6, 3           ; prologue
+
+        LDR R0, R6, 4
+        AND R0, R0, R0
+        BRnz hanoi0             ; if (nb_disques <= 0) goto hanoi0
+
+        ADD R0, R0, -1          ; R0 <- nb_disques - 1
+
+        LDR R1, R6, 5           ; R1 <- src
+        LDR R2, R6, 6           ; R2 <- tgt
+        ADD R3, R1, R2
+        NOT R3, R3
+        ADD R3, R3, 7           ; itm = R3 <- 6 - src - dst
+
+        ADD R6, R6, -3
+        STR R0, R6, 0
+        STR R1, R6, 1
+        STR R3, R6, 2
+        JSR hanoi              ; hanoi(nb_disques - 1, src, itm)
+
+        STR R1, R6, 0
+        STR R2, R6, 1
+        JSR mvt                ; mvt(src, tgt)
+
+        STR R0, R6, 0
+        STR R3, R6, 1
+        STR R2, R6, 2
+        JSR hanoi              ; hanoi(nb_disques - 1, itm, tgt)
+        ADD R6, R6, 3
+
+hanoi0: LDR R2, R6, 3           ; prologue
+        LDR R1, R6, 2
+        LDR R0, R6, 1
+        LDR R7, R6, 0
+        ADD R6, R6, 4
+        RET
+
+char0:  .FILL 48                ; code de '0'
+charNL: .FILL 10                ; code de '\n'
+arrow:  .STRINGZ " --> "
+
+sttop:  .BLKW 100
+stbot:
+
+        .END
