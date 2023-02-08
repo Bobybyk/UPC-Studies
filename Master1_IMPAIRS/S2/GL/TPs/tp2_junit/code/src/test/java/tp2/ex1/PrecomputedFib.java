879644b8e1667207c5ee1b4f776f8de4ea30585a
Matthieu Le Franc <matt.lefranc@gmail.com>
2023-02-08 18:06:12 +0100

    GL TP2

---
+++ Master1_IMPAIRS/S2/GL/TPs/tp2_junit/code/src/test/java/tp2/ex1/PrecomputedFib.java
@@ -0,0 +1,18 @@
+package tp2.ex1;
+
+public class PrecomputedFib {
+
+    // Le nombre d'éléments de la suite de Fibonacci à précalculer.
+    final int N = 30;
+
+    // Le tableau contenant les éléments précalculés.
+    int elements[] = new int[N];
+    
+    public PrecomputedFib() {
+        
+    }
+    
+    public int fib(int n) {
+        return n;
+    }
+}
--- Master1_IMPAIRS/S2/GL/TPs/tp2_junit/code/src/test/java/tp2/ex1/TestFib.java
+++ Master1_IMPAIRS/S2/GL/TPs/tp2_junit/code/src/test/java/tp2/ex1/TestFib.java
@@ -1,12 +1,23 @@
-import org.junit.jupiter.api.Test;
+package tp2.ex1;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import tp2.ex1.Fib;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestFib {
-
-    @tp2.ex1.Test
+    
+   @Test
     public void testFib() {
-        assertEquals(377, Fib.fib(15));
+        assertEquals(610, Fib.fib(15));   
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 12, 15, 20, 30})
+    public void testFib(int n) {
+        assertEquals(Fib.fib(n), Fib.fib(n));
     }
 
 }
+
