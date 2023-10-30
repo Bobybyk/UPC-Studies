4279ce1ed9e85beef32fc7aa1b23921001f2164d
Bobybyk <matt.lefranc@gmail.com>
2023-10-30 12:24:38 +0100

    ham

--- master/M2/Fouille/TPs/tp3_4/td4.py
+++ master/M2/Fouille/TPs/tp3_4/td4.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 def split_lines(input, seed, output1, output2):
     random.seed(seed)
@@ -11,6 +12,38 @@ def split_lines(input, seed, output1, output2):
             output2.write(line)
     output1.close()
     output2.close()
+
+def tokenize_and_split_2(sms_file) -> tuple:
+    words = {}
+    l1 = []
+    l2 = []
+    i = 0
+    for line in open(sms_file, 'r').readlines():
+        if line.startswith('ham'):
+            isSpam = False
+        else:
+            isSpam = True
+        tmpL1 = []
+        tmpL2 = []
+        
+        words_in_line = re.findall(r'\b\w+\b', line)
+
+        for word in words_in_line:
+            word = word.lower()
+            if word != 'ham' and word != 'spam':
+                if word not in words:
+                    words[word] = i
+                    i += 1
+                if isSpam:
+                    tmpL1.append(words[word])
+                else:
+                    tmpL2.append(words[word])
+        if len(tmpL1) != 0:
+            l1.append(tmpL1)
+        if len(tmpL2) != 0:
+            l2.append(tmpL2)
+
+    return (words, l1, l2)
 
 def tokenize_and_split(sms_file) -> tuple:
     words = {}
@@ -49,6 +82,32 @@ def compute_frequencies(num_words, documents):
     for index in range(len(freq)):
         freq[index] /= len(documents)
     return freq
+
+def naive_bayes_train_ham(sms_file):
+    words, l1, l2 = tokenize_and_split_2(sms_file)
+    ham_ratio = len(l2) / (len(l1) + len(l2))
+    ham_freq = compute_frequencies(len(words), l2)
+    union_freq = compute_frequencies(len(words), l1+l2)
+    hamcity = []
+    for i in range(len(words)):
+        if ham_freq[i] == 0:
+            hamcity.append(0)
+        else:
+            hamcity.append(ham_freq[i] / (union_freq[i]))
+    return (ham_ratio, words, hamcity)
+
+def naive_bayes_train_2(sms_file):
+    words, l1, l2 = tokenize_and_split_2(sms_file)
+    spam_ratio = len(l1) / (len(l1) + len(l2))
+    spam_freq = compute_frequencies(len(words), l1)
+    union_freq = compute_frequencies(len(words), l1+l2)
+    spamicity = []
+    for i in range(len(words)):
+        if spam_freq[i] == 0:
+            spamicity.append(0)
+        else:
+            spamicity.append(spam_freq[i] / (union_freq[i]))
+    return (spam_ratio, words, spamicity)
 
 def naive_bayes_train(sms_file):
     words, l1, l2 = tokenize_and_split(sms_file)
@@ -104,6 +163,8 @@ def naive_bayes_eval(test_sms_file, f):
 split_lines("SMSSpamCollection", 65157458, "data_train", "data_test")
 
 train_spam_ratio, train_words, train_spamicity = naive_bayes_train("data_train")
+train_spam_ratio_2, train_words_2, train_spamicity_2 = naive_bayes_train_2("data_train")
+train_ham_ratio, train_words_ham, train_hamicity = naive_bayes_train_ham("data_train")
 
 def classify_spam(sms):
     return naive_bayes_predict(train_spam_ratio, train_words, train_spamicity, sms) > 0.5
@@ -111,4 +172,9 @@ def classify_spam(sms):
 def classify_spam_precision(sms):
     return naive_bayes_predict(train_spam_ratio, train_words, train_spamicity, sms) > 0.9
 
-print(naive_bayes_eval("data_test", classify_spam_precision))
+def classify_spam_recall(sms):
+    return naive_bayes_predict(train_spam_ratio_2, train_words_2, train_spamicity_2, sms) > 0.9
+
+print("classify_spam_recal : ", naive_bayes_eval("data_test", classify_spam_recall))
+print("classify_spam_precision : ", naive_bayes_eval("data_test", classify_spam_precision))
+print("classify_spam : ", naive_bayes_eval("data_test", classify_spam))
