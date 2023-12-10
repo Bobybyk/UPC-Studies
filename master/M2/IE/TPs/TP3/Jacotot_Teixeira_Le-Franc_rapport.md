63ea16417083ee005f57d5fd6d0ea76290422b50
Bobybyk <matt.lefranc@gmail.com>
2023-12-10 16:38:07 +0100

    Fix oddOrPos method and add test cases

--- master/M2/IE/TPs/TP3/Jacotot_Teixeira_Le-Franc_rapport.md
+++ master/M2/IE/TPs/TP3/Jacotot_Teixeira_Le-Franc_rapport.md
@@ -1,5 +1,7 @@
 *Hugo Jacotot : 71802786*
+
 *Gabriel Teixeira : 21950970*
+
 *Matthieu Le Franc : 71800858*
 
 ## Exercice 1
@@ -70,6 +72,78 @@ Dans ce scénario, le tableau contient un zéro à l'indice 0, et le défaut (qu
 Cet état est erroné car la fonction cherche des zéros à l'indice 0, alors que pour trouver le dernier zéro, elle devrait commencer à l'indice de fin du tableau.
 
 ## Exercice 2
+
+```java
+public static int oddOrPos (int[] x) {
+    int count = 0;
+    for (int i = 0; i < x.length; i++) {
+        if(x[i]%2==1 || x[i]>0){
+            System.out.println(x[i]);
+            count++;
+        }
+    }
+    return count;
+}
+```
+
+**1/. Expliquez ce qui ne va pas avec le code donné. Décrivez le défaut précisément en proposant une modification du code.**
+
+La condition actuelle ne tient pas compte des nombres négatifs impairs.
+Par exemple, lastZero ({-2,0,1,4}) est correctement évalué à 2, tandis que numZero ({-3,-2,0,1,4}) est évalué incorrectement à 3 au lieu de 4.
+
+
+```java
+public static int oddOrPos_correction(int[] x) {
+    int count = 0;
+    for (int i = 0; i < x.length; i++) {
+        if (x[i] % 2 == 1|| x[i]%2==-1 || x[i] > 0) {
+            System.out.println(x[i]);
+            count++;
+        }
+    }
+    return count;
+}
+```
+
+**2/. Si possiblez, donnez un scénario de test qui n'exécute pas le défaut. Sinon, expliquez brièvement pourquoi.**
+
+x1 = {-2, -4, 0} dans ce scénario, le tableau ne contient que des nombres négatifs et zéro, donc la condition actuelle ne sera jamais vraie, et le défaut ne sera pas exécuté.
+
+```java
+@Test
+public void testQuestion2() {
+    int[] x1 = {-2, -4, 0};
+    assertEquals(0, oddOrPos(x1));
+}
+```
+
+**3/. Si possible, donnez un scénario de test qui exécute le défaut, sans l'apparition d'une erreur. Sinon, expliquez brièvement pourquoi.**
+
+Il n'existe pas de cas test passant par le défaut et n'executant pas l'erreur. Cet état est erroné précisément parce que la condition de la boucle est fausse donc is on rentre le if le défaut et l'erreur sont executés.
+
+**4/. Si possible, donnez une scénario de test qui entraîne une erreur, mais sans une défaillance visible. Sinon, expliquez brièvement pourquoi.**
+
+x = {1,2,3} un tableau avec des termes positif, et pas de termes négatifs impairs executera le défaut et donc l'erreur, mais en regardant la valeur retournée on ne le remarque pas puisqu'il retournera bien le nombre de valeur positf ou impairs et on ne voit donc pas la défaillance.
+
+```java
+@Test
+public void testQuestion4() {
+    int[] x1 = {1, 2, 3};
+    assertEquals(3, oddOrPos(x1));
+}
+```
+
+**5/. Pour le scénario de test donné, décrivez le premier état d'erreur. Assurez-vous de décrire l'état complet (avec le program counter)**
+
+Pour le scénario de test donné, décrivez le premier état d’erreur. Assurez-vous de décrire l’état complet (avec le program counter).
+
+Le défaut sera exécuté dès la première itération de la boucle.
+
+Premier état d'erreur :
+- x = {-3, -2, 0, 1, 4}
+- i = 0
+- PC = "x[i] % 2 == 1 || x[i] > 0"
+Cet état est erroné car la condition actuelle ne tient pas compte des nombres négatifs impairs.
 
 ## Exercice 3
 
--- master/M2/IE/TPs/TP3/TPOddOrPos.java
+++ master/M2/IE/TPs/TP3/TPOddOrPos.java
@@ -1,8 +1,7 @@
-/**
- * TP_oddOrPos
- */
-public class TPOddOrPos {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
     
     /*
      * 1 Expliquez ce qui ne va pas avec le code donné. Décrivez le défaut
@@ -24,11 +23,54 @@ public class TPOddOrPos {
      *  ->
      * 
      */
+
+public class TPOddOrPos {
+
+        /*
+                1)Expliquez ce qui ne va pas avec le code donné. Décrivez le défaut
+                précisément en proposant une modification du code.
+                2) Si possible, donnez un scénario de test qui n’exécute pas le défaut.
+                Sinon, expliquez brièvement pourquoi.
+                3) Si possible, donnez un scénario de test qui exécute le défaut, sans
+                l’apparition d’une erreur. Sinon, expliquez brièvement pourquoi.
+                4) Si possible, donnez un scénario de test qui entraîne une erreur, mais
+                sans une défaillance visible. Sinon, expliquez brièvement pourquoi.
+                5) Pour le scénario de test donné, décrivez le premier état d’erreur.
+                Assurez-vous de décrire l’état complet (avec le program counter).
+
+                ENONCE
+        */
     public static int oddOrPos (int[] x)
     {
         int count = 0;
         for (int i = 0; i < x.length; i++) {
-            if(x[i]%2==1 || x[i]%2==-1 || x[i]>0){
+            if(x[i]%2==1 || x[i]>0){
+                System.out.println(x[i]);
+                count++;
+            }
+        }
+        return count;
+    }
+        /*
+                MAIN
+        */
+        
+    public static void main(String[] args) {
+        int[] x1 = {-3,-2,0,1,4};
+        System.out.println(oddOrPos(x1));
+    }
+
+        
+        /*
+        Question 1)
+        -> La condition actuelle ne tient pas compte des nombres négatifs impairs.
+        Par exemple, lastZero ({-2,0,1,4}) est correctement évalué à 2, tandis
+        que numZero ({-3,-2,0,1,4}) est évalué incorrectement à 3 au lieu de 4.
+        */        
+    public static int oddOrPos_correction(int[] x) {
+        int count = 0;
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] % 2 == 1|| x[i]%2==-1 || x[i] > 0) {
                 System.out.println(x[i]);
                 count++;
             }
@@ -36,8 +78,44 @@ public class TPOddOrPos {
         return count;
     }
 
-    public static void main(String[] args) {
-        int[] x1 = {-3,-2,0,1,4};
-        System.out.println(oddOrPos(x1));
-    }
+        /*
+        Question 2)
+        -> x1 = {-2, -4, 0} dans ce scénario, le tableau ne contient que des nombres négatifs et zéro,
+        donc la condition actuelle ne sera jamais vraie, et le défaut ne sera pas exécuté.
+        */
+        @Test
+        public void testQuestion2() {
+                int[] x1 = {-2, -4, 0};
+                assertEquals(0, oddOrPos(x1));
+        }
+
+        /*
+        Question 3)
+        Il n'existe pas de cas test passant par le défaut et n'executant pas l'erreur.
+        Cet état est erroné précisément parce que la condition de la boucle est fausse donc is on rentre le if le défaut et
+        l'erreur sont executés.
+        */
+
+        /*
+        Question 4)
+        -> x = {1,2,3} un tableau avec des termes positif, et pas de termes négatifs impairs executera le défaut et donc l'erreur,
+        mais en regardant la valeur retournée on ne le remarque pas puisqu'il retournera bien le nombre de valeur positf ou impairs et on
+        ne voit donc pas la défaillance.
+        */
+        @Test
+        public void testQuestion4() {
+                int[] x1 = {1, 2, 3};
+                assertEquals(3, oddOrPos(x1));
+        }
+
+        /*
+        5) Pour le scénario de test donné, décrivez le premier état d’erreur.
+           Assurez-vous de décrire l’état complet (avec le program counter).
+        Le défaut sera exécuté dès la première itération de la boucle.
+        Premier état d'erreur :
+        - x = {-3, -2, 0, 1, 4}
+        - i = 0
+        - PC = "x[i] % 2 == 1 || x[i] > 0"
+        Cet état est erroné car la condition actuelle ne tient pas compte des nombres négatifs impairs.
+        */
 }
